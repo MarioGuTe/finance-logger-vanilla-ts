efaf1e2920862a5674df96e6e7f855f3e6bf04eb
MarioGuTe <mariogutierreztello@gmail.com>
2023-09-23 23:27:39 -0300

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,50 @@
+// *** Types ***
+// let character = "Mario";
+// let age = 30;
+// let isBlackBelt = false;
+
+// character = "Rodrigo";
+
+// const circunference = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+
+// console.log(circunference(7.5));
+
+//***** Arrays and objects *****
+// Arrays
+// Ex1
+// let names = ["Mario", "Sebastián", "Rodrigo"];
+
+// This doesn't work because the original array only contains string data-type
+// names.push(15);
+
+// names.push("Daniel");
+// console.log(names);
+
+// Ex2
+// let MixedTypes = ["Mario", 34, "Rodrigo", 30];
+
+// Both of these work because originally the array contains "string" an "number" as data-types
+// MixedTypes.push("Sebastián");
+// MixedTypes.push(15);
+
+// console.log(MixedTypes);
+
+// Objects
+
+// let person = {
+//   name: "Mario",
+//   hobby: "music",
+//   age: 34,
+// };
+
+// Works
+// person.name = "Rodrigo";
+
+// Doesn't work
+// person.hobby = true;
+// person.age = "thirty";
+// person.skills = "Kung Fu";
+
+// console.log(person.name, person.hobby, person.age);
